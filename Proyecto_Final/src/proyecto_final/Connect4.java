5a7a12fda2abd1c2e1cab1a782d89390cb85f48b
Hrivera2004 <hrolriverav@gmail.com>
2023-12-07 10:09:12 -0600

    cambios

--- Proyecto_Final/src/proyecto_final/Connect4.java
+++ Proyecto_Final/src/proyecto_final/Connect4.java
@@ -1,17 +1,26 @@
 
 package proyecto_final;
     import java.util.*;
+    import javax.swing.JOptionPane;
 
 public class Connect4 {
     Random rd = new Random();
-    
+    static Scanner sc = new Scanner(System.in);
+    boolean campeon=false;
+    int turn;
     int x;
     int y;
     int[][] tab;
     
-    public Connect4(int[][] tab, int x, int y) {
+    public Connect4(){
+    }
+    public Connect4(int[][] tab,int x, int y,int turn) {
+        this.tab = tab;
         this.x = x;
         this.y = y;
+        this.turn = turn;
+    }
+    public Connect4(int[][] tab) {
         this.tab = tab;
     }
     
@@ -24,6 +33,9 @@ public class Connect4 {
     public int getY() {
         return y;
     }
+    public int getTurn() {
+        return turn;
+    }
     
     public void setY(int y) {
         this.y = y;
@@ -34,21 +46,145 @@ public class Connect4 {
     public void setTab(int[][] tab) {
         this.tab = tab;
     }
+    public void setTurn(int turn) {
+        this.turn = turn;
+    }
 
     
+    public void c4(){
+        options();
+    }
     
+    public void options(){
+        String[] choose = {"2 Players","AI"};
+        int option=JOptionPane.showOptionDialog(null, "Eliga la modalidad de juego"
+                ,"Connect4", 0, 3, null, choose, choose[0]);
+        turn=0;
+        while(turn<42 && campeon==false){ 
+            turn++;
+            play(option);
+            System.out.println("------------------------");
+            flip();
+        }
+        if (campeon==true) {
+            if (turn%2!=0) {
+                System.out.println("Jugador 1 gano");
+            }else if(option==0){
+                System.out.println("Jugador 2 gano");
+            }else{
+                System.out.println("AI gano");
+            }
+        }
+    }
+    
+    public void play(int option){
+        if (option==0) {
+            if (turn%2!=0) {
+                System.out.println("Turno jugador 1");
+                jugador1();
+                print(tab);
+                System.out.println("-----------------");
+            }else{
+                System.out.println("Turno jugador 2");
+                jugador2();
+                print(tab);
+                System.out.println("-----------------");
+            }
+            if (turn>=0) {
+                System.out.println("-------------");
+                System.out.println(x+" :x");
+                System.out.println(y+" :y");
+                System.out.println("-------------");
+                //campeon=Ver(tab,x,y);
+            }
+            
+        }else{
+            if (turn%2!=0) {
+                System.out.println("Turno jugador 1");
+                jugador1();
+                print(tab);
+                System.out.println("-----------------");
+            }else{
+                System.out.println("Turno AI");
+                AI();
+                print(tab);
+                System.out.println("-----------------");
+            }
+            if (turn>=0) {
+                System.out.println("-------------");
+                System.out.println(x+" :x");
+                System.out.println(y+" :y");
+                System.out.println("-------------");
+                //campeon=Ver(tab,x,y);
+            }
+        }
+    }    
+    public void jugador1(){
+        System.out.println("Ingrese columna(1-7): ");
+        x = sc.nextInt()-1;
+        boolean found=false;
+        while(found==false){
+            for (int i = 5; i >= 0 && found==false; i--) {
+                if (tab[i][x]==0) {
+                    found=true;
+                    tab[i][x]=1;
+                    y=i;
+                }
+            }
+            if (found==false) {
+                System.out.println("No se encontro el espacio que desea ingrese ptra columna");
+                x = sc.nextInt()-1;
+            }
+        }
+        
+    }
+    public void jugador2(){
+        System.out.println("Ingrese columna(1-7): ");
+        x = sc.nextInt()-1;
+        boolean found=false;
+        while(found==false){
+            for (int i = 5; i >= 0 && found==false; i--) {
+                if (tab[i][x]==0) {
+                    found=true;
+                    tab[i][x]=2;
+                    y=i;
+                }
+            }
+            if (found==false) {
+                System.out.println("No se encontro el espacio que desea ingrese ptra columna");
+                x = sc.nextInt()-1;
+            }
+        }
+    }
+    public void AI(){
+        x = rd.nextInt(7);
+        boolean found=false;
+        while(found==false){
+            for (int i = 5; i >= 0 && found==false; i--) {
+                if (tab[i][x]==0) {
+                    found=true;
+                    tab[i][x]=2;
+                    y=i;
+                }
+            }
+            if (found==false) {
+                x = rd.nextInt(7);
+            }
+        }
+    }
     public boolean Ver(int[][] mat, int x, int y) {
         ArrayList<Integer> pos = pos(tab,x,y);
-        int diaP1=0,diaP2=0,diaN1 = 0,diaN2 = 0,rectH1=0,rectH2=0,rectV=0;
         
+        int diaP1=0,diaP2=0,diaN1 = 0,diaN2 = 0,rectH1=0,rectH2=0,rectV=0;
         for (int i = 0; i < pos.size(); i++) {
             int win = pos.get(i);
-
+            System.out.println("----------------------"+win);
             boolean proximity=true;
-            
             if (win==1) {
-                for (int j = 0; j < 4 && proximity; j++) {
-                    if (mat[y][x]==mat[y-j][x+j]) {
+                for (int j = 0; j < 4 ; j++) {
+                    
+                    if (mat[y][x]==mat[y-j][x+j] &&  proximity==true) {
+                        System.out.println("1");
                         diaP1++; 
                     }else{
                         proximity=false;
@@ -56,8 +192,10 @@ public class Connect4 {
                     
                 }
             }else if(win==2) {
-                for (int j = 0; j < 4 && proximity; j++) {
-                    if (mat[y][x]==mat[y][x+j]) {
+                for (int j = 0; j < 4 ; j++) {
+                    
+                    if (mat[y][x]==mat[y][x+j]&& proximity==true) {
+                        System.out.println("2");
                         rectH1++; 
                     }else{
                         proximity=false;
@@ -65,8 +203,10 @@ public class Connect4 {
                     
                 }
             }else if(win==3) {
-                for (int j = 0; j < 4&&proximity; j++) {
-                    if (mat[y][x]==mat[y+j][x+j]) {
+                for (int j = 0; j < 4; j++) {
+                    
+                    if (mat[y][x]==mat[y+j][x+j]&&proximity==true) {
+                        System.out.println("3");
                         diaN1++; 
                     }else{
                         proximity=false;
@@ -74,8 +214,10 @@ public class Connect4 {
                     
                 }
             }else if(win==4) {
-                for (int j = 0; j < 4&& proximity; j++) {
-                    if (mat[y][x]==mat[y+j][x]) {
+                for (int j = 0; j < 4; j++) {
+                    
+                    if (mat[y][x]==mat[y+j][x]&& proximity==true) {
+                        System.out.println("4");
                         rectV++; 
                     }else{
                         proximity=false;
@@ -83,8 +225,10 @@ public class Connect4 {
                     
                 }
             }else if(win==5) {
-                for (int j = 0; j < 4 && proximity; j++) {
-                    if (mat[y][x]==mat[y+j][x-j]) {
+                for (int j = 0; j < 4; j++) {
+                    
+                    if (mat[y][x]==mat[y+j][x-j]&&proximity==true) {
+                        System.out.println("5");
                         diaP2++; 
                     }else{
                         proximity=false;
@@ -92,8 +236,9 @@ public class Connect4 {
                     
                 }
             }else if(win==6) {
-                for (int j = 0; j < 4&& proximity; j++) {
-                    if (mat[y][x]==mat[y][x-j]) {
+                for (int j = 0; j < 4; j++) {
+                    if (mat[y][x]==mat[y][x-j]&&proximity==true) {
+                        System.out.println("6---------");
                         rectH2++; 
                     }else{
                         proximity=false;
@@ -101,8 +246,10 @@ public class Connect4 {
                     
                 }
             }else if(win==7) {
-                for (int j = 0; j < 4 && proximity; j++) {
-                    if (mat[y][x]==mat[y-j][x-j]) {
+                for (int j = 0; j < 4; j++) {
+                    
+                    if (mat[y][x]==mat[y-j][x-j]&&proximity==true) {
+                        System.out.println("7----------");
                         diaN2++; 
                     }else{
                         proximity=false;
@@ -112,12 +259,11 @@ public class Connect4 {
             }
             
         }
-
+        System.out.println("---------------------------");
         
         int diaP= diaP1+diaP2 ;
         int diaN = diaN1+diaN2;
         int rectH= rectH1+rectH2;
-        
         
         if (diaN<4&&diaP<4&&rectH<4) {
             int[] rem = {4-diaN,4-diaP,4-rectH};
@@ -180,64 +326,73 @@ public class Connect4 {
                 }
             }
         }
+         System.out.println("--------VErificacion-------");
         if (diaN>=4||diaP>=4||rectH>=4||rectV>=4) {
             return true;
         }else{
             return false;
         }
-    }
-    
+       
+    }//hector
     public ArrayList<Integer> pos(int[][] mat, int x, int y) {
         ArrayList<Integer> pos = new ArrayList<Integer>();
         if (mat.length+1-x>3 && y>3) {//diagonal positiva
             pos.add(1);
+            System.out.println("1+");
         }
         if (mat.length+1-x>3) {//recta horizontal positiva
             pos.add(2);
+            System.out.println("2+");
         }
         if (mat.length+1-x>3 && mat.length-y>3) {//diagonal inversa negativa 
             pos.add(3);
+            System.out.println("3+");
         }
         if (mat.length-y>3) {//recta vertical 
             pos.add(4);
+            System.out.println("4+");
         }
         if (x>3 && mat.length-y>3) {//diagonal Abajo negativa 
             pos.add(5);
+            System.out.println("5+");
         }
         if (x>3) {//recta horizontal negativa 
             pos.add(6);
+            System.out.println("6+");
         }
         if (x>3 && y>3) {//diagonal Arriba negativa
             pos.add(7);
+            System.out.println("7+");
         }
         return pos;
-    }
-    
-    public void flip(int[][] tab,int x){
+    }//hector
+    public void flip(){
         
         int chance = rd.nextInt(101);
         ArrayList<Integer> flip = new ArrayList<Integer>();
         
-        if (chance<200) {
+        if (chance<5) {
             boolean ver = true;
             for (int i = 0; i<6 ; i++) {
                 if (tab[tab.length-1-i][x]==0) {
                 }else{
                     flip.add(tab[tab.length-1-i][x]);
-                    System.out.println(tab[tab.length-1-i][x]);
                 }
                     
             }
-            System.out.println(flip.size());
             int cont=0;
             for (int i = tab.length-flip.size(); i <= tab.length-1; i++) {
                 int num = flip.get(cont);
                 tab[i][x]= num;
                 cont++;
             }
+            JOptionPane.showMessageDialog(null, "Hubo un flip en"+x);
+            System.out.println("-------------------");
+            print(tab);
+            System.out.println("-------------------");
         }
-        print(tab);
     }
+
     public void print(int[][] x) {
         for (int i = 0; i < x.length; i++) {
             for (int j = 0; j < x[i].length; j++) {
--- Proyecto_Final/src/proyecto_final/GUI_C4.java
+++ Proyecto_Final/src/proyecto_final/GUI_C4.java
@@ -26,21 +26,40 @@ public class GUI_C4 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("jButton1");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(650, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(420, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -78,5 +97,6 @@ public class GUI_C4 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
