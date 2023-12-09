7894bae2d1d50978f496d537d9eb1beda88014db
gsussman05 <147879065+gsussman05@users.noreply.github.com>
2023-12-09 16:08:34 -0600

    Add files via upload

---
+++ Proyecto_Gui/src/proyecto_gui/Proyecto_Gui.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_gui;
+import java.util.Scanner;
+import java.util.Random;
+/**
+ *
+ * @author Gabriel
+ */
+public class Proyecto_Gui {
+static Scanner sc=new Scanner(System.in);
+static Random rand=new Random();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VisualGUI gui=new VisualGUI();
+        gui.setVisible(true);
+    }
+    
+}
